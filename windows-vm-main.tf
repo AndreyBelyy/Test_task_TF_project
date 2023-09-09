e27f57d3154c9ba886f9d1def1e841549daa4a52
Andrei Belyi <andreybelyyw@gmail.com>
2023-09-09 17:11:47 -0600

    All files

---
+++ windows-vm-main.tf
@@ -0,0 +1,167 @@
+data "template_file" "windows_data" {
+  template = <<-EOF
+    <powershell>
+    # Rename Machine
+    Rename-Computer -NewName "${var.windows_instance_name}" -Force;
+    
+    # New user
+    New-LocalUser -Name 'Tomas' -Description 'Description of this account.' -Password 'Edison'
+    
+    # Add to RDP
+    Add-LocalGroupMember -Group "Remote Desktop Users" -Member "Tomas"
+
+    # Install Chocolatey (package manager for Windows)
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+    # Install Skype and Telegram
+    choco install -y skype telegram
+    
+    # Restart machine
+    shutdown -r -t 10;
+    </powershell>
+    EOF
+}
+
+# Define the security group for the Windows server
+resource "aws_security_group" "aws-windows-sg" {
+  name        = "windows-sg"
+  description = "Allow incoming connections"
+  vpc_id      = aws_vpc.vpc.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming HTTP connections"
+  }
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming RDP connections"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming HTTPS connections"
+  }
+
+  ingress {
+    from_port   = 3478
+    to_port     = 3478
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming Skype UDP connections"
+  }
+  ingress {
+    from_port   = 3479
+    to_port     = 3479
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming Skype UDP connections"
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Specific IP addresses recommended
+    description = "Allow incoming HTTP connections"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow outcoming HTTP connections"
+  }
+  egress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow outcoming RDP connections"
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow outcoming HTTPS connections"
+  }
+  egress {
+    from_port   = 3478
+    to_port     = 3478
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow outcoming Skype UDP connections"
+  }
+  egress {
+    from_port   = 3479
+    to_port     = 3479
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow outcoming Skype UDP connections"
+  }
+  tags = {
+    Name = "windows-sg"
+  }
+}
+
+# Create EC2 Instance
+resource "aws_instance" "windows-server" {
+  ami                         = "ami-0e58466bb52b01adb"
+  instance_type               = var.windows_instance_type
+  subnet_id                   = aws_subnet.public-subnet.id
+  vpc_security_group_ids      = [aws_security_group.aws-windows-sg.id]
+  source_dest_check           = false
+  key_name                    = aws_key_pair.key_pair.key_name
+  user_data                   = data.template_file.windows-userdata.rendered
+  associate_public_ip_address = var.windows_associate_public_ip_address
+
+  # root disk
+  root_block_device {
+    volume_size           = var.windows_root_volume_size
+    volume_type           = var.windows_root_volume_type
+    delete_on_termination = true
+    encrypted             = true
+  }
+  # extra disk
+  ebs_block_device {
+    device_name           = "/dev/xvda"
+    volume_size           = var.windows_data_volume_size
+    volume_type           = var.windows_data_volume_type
+    encrypted             = true
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name        = "windows-server-vm"
+    Environment = var.app_environment
+  }
+}
+
+# Create Elastic IP for the EC2 instance
+resource "aws_eip" "windows-eip" {
+  vpc  = true
+  tags = {
+    Name = "windows-eip"
+  }
+}
+# Associate Elastic IP to Windows Server
+resource "aws_eip_association" "windows-eip-association" {
+  instance_id   = aws_instance.windows-server.id
+  allocation_id = aws_eip.windows-eip.id
+}
+
+
